1883a7caa21a037428c49be275c6be6d9638855e
satsathish999 <40082267+satsathish999@users.noreply.github.com>
2019-06-04 09:56:53 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
-a=int(input())
-if(a==0):
-  print "Zero"
-elif(a<0):
-  print "Negative"
+ac=int(input())
+if(ac==0):
+  print("Zero")
+elif(ac<0):
+  print("Negative")
 else:
-  print "Positive"
+  print("Positive")
