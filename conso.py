0e3896e3aa97b2823c34c53eba506b00d41eb4e2
satsathish999 <40082267+satsathish999@users.noreply.github.com>
2019-06-04 10:31:54 +0530

    conso.py

---
+++ conso.py
@@ -0,0 +1,7 @@
+ac=input()
+if(ac=="a" or ac=="e" or ac=="i" or ac=="o" or ac=="u" or ac=="A" or ac=="E" or ac=="I" or ac=="O" or ac=="U"):
+      print("Vowel")
+elif(ac.isalpha()==True):
+      print("Consonant")
+else:
+     print("invalid")
